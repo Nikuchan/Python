3312175d4bc1299b70a74a64011f3f7c402eaabc
Nikuchan <36480832+Nikuchan@users.noreply.github.com>
2018-12-04 07:04:12 +0530

    Update ListsAndTuples.py

--- ListsAndTuples.py
+++ ListsAndTuples.py
@@ -1,18 +1,19 @@
 # Lists
 courses=['History','Math','Physics','Compsci']
 courses_2=['Football','Basketball']
+print("Slicing Examples")
 print(len(courses))
 print(courses)
 print(courses[-1])
 print(courses[0:3])
 print(courses[::-1])
 
-# Add
+print("\nAdd")
 courses.append('Art')
 print(courses)
 
 
-#Insert at the beginning
+print("\nInsert at the beginning")
 courses.insert(0,'Science')
 print(courses)
 
@@ -22,24 +23,24 @@ print(courses)
 #print(courses[0])
 
 
-# Append we use to add individual items , Extend we use to add another list in the form of individual items
+print("\nAppend we use to add individual items , Extend we use to add another list in the form of individual items")
 courses.extend(courses_2)
 print(courses)
 
-#remove
+print("\nremove")
 courses.remove('Math')
 print(courses)
 
-# To remove the last value
+print("\nTo remove the last value")
 popped=courses.pop()
 print(courses)
-print ("popped value from Courses is " +popped)
+print (f"popped value is {popped}")
 
-#Reverse
+print("Reverse")
 courses.reverse()
 print(courses)
 
-#Sort
+print("\nSort")
 courses.sort()
 print(courses)
 
@@ -47,69 +48,48 @@ num=[4,21,54,1,34]
 num.sort()
 print(num)
 
-#Sort in descending Order
+print("\nSort in descending Order")
 courses.sort(reverse=True)
 num.sort(reverse=True)
 print(courses)
 print(num)
 
 
-# Sorting the list without altering the original list using the function sorted other than the sort method
+print("\nSorting the list without altering the original list using the function sorted other than the sort method")
 Sorted_courses=sorted(courses)
 print(courses)
 print(Sorted_courses)
 
-
+print("\nFinding Min and Max Values")
 print(min(num))
 print(max(num))
 print(sum(num))
+
+print("\nFinding the Index\n")
 print(courses.index('Compsci'))
 
+print('Print("Art in Courses")')
 print('Art' in courses)
 
-
+print("\nUsing for Loop")
 for course in courses:
     print(course)
 
 # TO get the index also
-
+print("\nGetting Index value also using for loop")
 for index,course in enumerate(courses):
     print(index,course)
 
-# TO start with 1 as the index
+print("\nTO start with 1 as the index")
 print("\n")
 for index,course in enumerate(courses,start=1):
     print(index,course)
 
-# turning the list into a string separated by some character
+print("\nturning the list into a string separated by some character")
 course_str=' - '.join(courses)
 
 print(course_str)
 
+print("\nConverting the string back to List")
 new_list=course_str.split(' - ')
 print(new_list)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
